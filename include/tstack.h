6919365af3b3efc4d68c86450505cbf324be3410
sorodan <alekseimetelkow@gmail.com>
2026-04-21 23:22:47 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -1,10 +1,36 @@
 // Copyright 2021 NNTU-CS
 #ifndef INCLUDE_TSTACK_H_
 #define INCLUDE_TSTACK_H_
+#include <string>
 
-template<typename T, int size>
+template<typename T, int Size>
 class TStack {
-  // добавьте код стека
+private:
+    T arr[Size];
+    int top;
+
+public:
+    TStack() :top(-1) {}
+    bool empty() const {
+        return top == -1;
+    }
+    bool full() const {
+        return top == Size - 1;
+    }
+    T get() const {
+        if (!empty())
+            return arr[top];
+        else
+            return 0;
+    }
+    void Pop() {
+        if (!empty())
+            top--;
+    }
+    void Push(T item) {
+        if (!full())
+            arr[++top] = item;
+    }
 };
 
-#endif  // INCLUDE_TSTACK_H_
+#endif  // INCLUDE_TSTACK_H_#pragma once
